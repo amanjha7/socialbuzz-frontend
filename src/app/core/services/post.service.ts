90745a8c3e5862e5fda818bfc3de5f5c958ae82a
Aman Jha <amanj@pronnel.com>
2025-01-01 22:36:00 +0530

    view post, add comment and like part is complete

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -3,16 +3,31 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
+  private apiUrl = 'http://localhost:8080/api/post'; // Base backend API URL
 
-  private apiUrl = 'http://localhost:8080/api/post/show-all';  // Backend API URL
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Method to fetch all posts
   getPosts(): Observable<any[]> {
-    return this.http.post<any[]>(this.apiUrl, {});  // Passing an empty object to the POST request
+    return this.http.post<any[]>(`${this.apiUrl}/show-all`, {}); // Replace URL if needed
+  }
+
+  // Method to like a post
+  likePost(postId: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${postId}/like`, {}); // Replace URL if needed
+  }
+
+  addComment(postId: string, content: string): Observable<any> {
+    const url = `${this.apiUrl}/${postId}/comment`;
+    return this.http.post<any>(url, { content });
+  }
+
+  // Method to get all comments for a post
+  getComments(postId: string): Observable<any[]> {
+    const url = `${this.apiUrl}/${postId}/comments`;
+    return this.http.get<any[]>(url);
   }
 }
--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/core/services/post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts: any[] = [];
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe(
+      (res) => {
+        console.log(res);
+        this.posts = res.map((post) => ({
+          ...post,
+          showComments: false, // Toggle visibility of comments
+          newComment: '', // For new comment input
+        }));
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
+  toggleComments(postId: string): void {
+    const post = this.posts.find((p) => p._id === postId);
+    if (post) {
+      post.showComments = !post.showComments;
+    }
+  }
+
+
+  toggleLike(postId: string): void {
+    this.postService.likePost(postId).subscribe(
+      (res) => {
+        const post = this.posts.find((p) => p._id === postId);
+        if (post) {
+          // Update the likes array dynamically
+          post.likes = res.likes;
+        }
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
+
+  addComment(postId: string): void {
+    const post = this.posts.find((p) => p._id === postId);
+    if (post && post.newComment.trim()) {
+      this.postService.addComment(postId, post.newComment).subscribe(
+        (res) => {
+          post.comments.push(res.comment); // Push the new comment to the post
+          post.newComment = ''; // Clear the input field
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    }
+  }
 }
