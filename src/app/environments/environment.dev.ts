14411d859dbb6b94dd69496a4b44e50e30a4577b
Aman Jha <amanj@pronnel.com>
2025-01-05 17:21:02 +0530

    Remove environment files from version control

--- src/app/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-// developement environment
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api',
-    userRoute: '/user',
-    postRoute: '/post',
-    authRoute: '/auth'
-};
--- src/app/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-// production environment
-export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:8080/api',
-    userRoute: '/user',
-    postRoute: '/post',
-    authRoute: '/auth'
-};
